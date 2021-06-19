056836e1958fe9e990ef4348bfb9bfc002a0cc82
sangwonyu <tkddnjs231@naver.com>
2021-06-19 21:01:45 +0900

    level1 : 다트게임 진행중

---
+++ level1/[1차]  다트 게임.js
@@ -0,0 +1,99 @@
+function solution(dartResult) {
+    let answer = [[], [], []];
+    let index = 0;
+    let point = 0;
+    let bonus = 0;
+    let option = 0;
+    const splitString = dartResult.split('');
+
+    for (const [splitIndex, value] of splitString.entries()) {
+        if (splitIndex > 0 && isNumber(splitString[splitIndex - 1])
+            && isNumber(value)) {
+            point = +(splitString[splitIndex - 1] + value);
+            continue;
+        }
+
+        if (isNumber(value)) {
+            if (point !== 0 ) {
+                const insertResult = insertAnswer(option, value, answer, index, point, bonus);
+                answer = insertResult.answer;
+                index = insertResult.index;
+                point = 0;
+                bonus = 0;
+                option = 0;
+            }
+
+            point = +value;
+        } else if (isBonus(value)) {
+            bonus = exChangeBonus(value);
+        } else if (isOption(value)) {
+            option = exChangeOption(value);
+        }
+        // 계산 조건 숫자(index !== 0), option
+
+        if(isOption(value)) {
+            console.log('bonus', bonus)
+            const insertResult = insertAnswer(option, value, answer, index, point, bonus);
+            answer = insertResult.answer;
+            index = insertResult.index;
+        }
+    }
+    console.log(answer);
+    return answer;
+}
+
+const isNumber = (value) => {
+    return !!(+value);
+}
+
+const isBonus = (value) => {
+    return value.includes('S') || value.includes('D') || value.includes('T');
+}
+
+const exChangeBonus = (value) => {
+    let bonus = 0;
+    switch(value) {
+        case 'S' :
+            bonus = 1;
+            break;
+        case 'D' :
+            bonus = 2;
+            break;
+        case 'T' :
+            bonus = 3;
+            break;
+    }
+    return bonus;
+}
+
+const isOption = (value) => {
+    return value.includes('#') || value.includes('*');
+}
+
+const exChangeOption = (value) => {
+    let option = 0;
+
+    switch (value) {
+        case '*' :
+            option = 2;
+            break;
+        case '#' :
+            option = -1;
+            break;
+    }
+
+    return option;
+}
+
+const insertAnswer = (option, value, answer, index, point, bonus) => {
+    console.log(bonus)
+    option = isNumber(value) ? 1 : option;
+    answer[index] = Math.pow(point, bonus) * option;
+    if (index === 1 && value === '*') {
+        answer[0] *= 2;
+    }
+    ++index
+    return {answer, index};
+}
+
+solution('1S2D*3T');
