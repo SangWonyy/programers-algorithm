90ba3d01145cff05a8d4aa58ad4f0407942e4b14
sangwonyu <tkddnjs231@naver.com>
2021-10-15 09:51:09 +0900

    level: 1 : 최소직사각형 re

--- level1/최소직사각형.js
+++ level1/최소직사각형.js
@@ -1,15 +1,26 @@
 function solution(sizes) {
-    const flatSize = sizes.flat();
-    const maxNumber = Math.max(...flatSize);
-    const includeMaxNumberList = sizes.filter((size) => size.includes(maxNumber));
-    const flatMaxNumberList = includeMaxNumberList.flat();
-    flatMaxNumberList.sort((a,b) => b - a);
-    const maxPairNumber = flatMaxNumberList[includeMaxNumberList.length];
-    flatSize.sort((a, b) => a - b);
-    const secondNumber = flatSize.find((size) => {
-        if (size >= maxPairNumber) return true;
-    })
+    const flatSizes = sizes.flat();
+    const maxNumber = Math.max(...flatSizes);
+    const maxPairList = sizes.filter((size) => size.includes(maxNumber));
+    const maxPair = maxPairList.reduce((acc, next) => {
+        if (acc[0] * acc[1] < next[0] * next[1] ) {
+            acc = next;
+            return acc;
+        } else {
+            return acc;
+        }
+    }, maxPairList[0]);
+    maxPair.sort((a, b) => a - b);
+    let secondNumber = maxPair[0];
+    sizes.forEach((size) => {
+        if (size === maxPair) return false;
 
-    console.log(flatSize)
+        size.sort((a, b) => a - b);
+        if (size[0] > secondNumber) {
+            secondNumber = size[0];
+        }
+    });
+
+
     return maxNumber * secondNumber;
 }
